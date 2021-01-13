0498c1ed04cdaa18f345d231c941b3812f4a5f54
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2021-01-13 10:35:33 +0100

    MMA-5080 La til pdl som env url.

--- core/src/main/java/no/nav/safselvbetjening/SafSelvbetjeningProperties.java
+++ core/src/main/java/no/nav/safselvbetjening/SafSelvbetjeningProperties.java
@@ -7,9 +7,20 @@ import lombok.ToString;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+
 @Data
 @ConfigurationProperties("safselvbetjening")
 @Validated
 public class SafSelvbetjeningProperties {
 
+    private final Endpoints endpoints = new Endpoints();
+
+    @Data
+    @Validated
+    public static class Endpoints {
+        @NotEmpty
+        private String pdl;
+    }
 }
