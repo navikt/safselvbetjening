989f1fec5c6aa7d8c25960072353eca5005c1474
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2021-03-05 13:53:03 +0100

    MMA-5112 Egen proxy config for azure consumeren.

--- core/src/main/java/no/nav/safselvbetjening/consumer/azure/AzureTokenConsumer.java
+++ core/src/main/java/no/nav/safselvbetjening/consumer/azure/AzureTokenConsumer.java
@@ -1,13 +1,19 @@
 package no.nav.safselvbetjening.consumer.azure;
 
 import no.nav.safselvbetjening.AzureProperties;
+import no.nav.safselvbetjening.SafSelvbetjeningProperties;
 import no.nav.safselvbetjening.cache.CacheConfig;
+import org.apache.http.HttpHost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.DefaultProxyRoutePlanner;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
@@ -20,38 +26,52 @@ import java.util.Collections;
 
 @Component
 public class AzureTokenConsumer {
-    private final RestTemplate restTemplate;
-    private final AzureProperties azureProperties;
+	private final RestTemplate restTemplate;
+	private final AzureProperties azureProperties;
 
-    public AzureTokenConsumer(AzureProperties azureProperties,
-                              RestTemplateBuilder restTemplateBuilder) {
-        this.restTemplate = restTemplateBuilder
-                .setConnectTimeout(Duration.ofSeconds(3))
-                .setReadTimeout(Duration.ofSeconds(20))
-                .build();
-        this.azureProperties = azureProperties;
-    }
+	public AzureTokenConsumer(SafSelvbetjeningProperties safSelvbetjeningProperties,
+							  AzureProperties azureProperties,
+							  RestTemplateBuilder restTemplateBuilder) {
+		final CloseableHttpClient httpClient = createHttpClient(safSelvbetjeningProperties.getProxy());
+		this.restTemplate = restTemplateBuilder
+				.setConnectTimeout(Duration.ofSeconds(3))
+				.setReadTimeout(Duration.ofSeconds(20))
+				.requestFactory(() -> new HttpComponentsClientHttpRequestFactory(httpClient))
+				.build();
+		this.azureProperties = azureProperties;
+	}
 
-    @Retryable(include = AzureTokenException.class)
-    @Cacheable(CacheConfig.AZURE_CLIENT_CREDENTIAL_TOKEN_CACHE)
-    public TokenResponse getClientCredentialToken() {
-        try {
-            HttpHeaders headers = createHeaders();
-            String form = "grant_type=client_credentials&scope=" + azureProperties.getScope() + "&client_id=" +
-                    azureProperties.getClientId() + "&client_secret=" + azureProperties.getClientSecret();
-            HttpEntity<String> requestEntity = new HttpEntity<>(form, headers);
+	private CloseableHttpClient createHttpClient(SafSelvbetjeningProperties.Proxy proxy) {
+		if(proxy.isSet()) {
+			final HttpHost proxyHost = new HttpHost(proxy.getHost(), proxy.getPort());
+			return HttpClients.custom()
+					.setRoutePlanner(new DefaultProxyRoutePlanner(proxyHost))
+					.build();
+		} else {
+			return HttpClients.createDefault();
+		}
+	}
 
-            return restTemplate.exchange(azureProperties.getTokenUrl(), HttpMethod.POST, requestEntity, TokenResponse.class)
-                    .getBody();
-        } catch (HttpClientErrorException | HttpServerErrorException e) {
-            throw new AzureTokenException(String.format("Klarte ikke hente token fra Azure. Feilet med httpstatus=%s. Feilmelding=%s", e.getStatusCode(), e.getMessage()), e);
-        }
-    }
+	@Retryable(include = AzureTokenException.class)
+	@Cacheable(CacheConfig.AZURE_CLIENT_CREDENTIAL_TOKEN_CACHE)
+	public TokenResponse getClientCredentialToken() {
+		try {
+			HttpHeaders headers = createHeaders();
+			String form = "grant_type=client_credentials&scope=" + azureProperties.getScope() + "&client_id=" +
+					azureProperties.getClientId() + "&client_secret=" + azureProperties.getClientSecret();
+			HttpEntity<String> requestEntity = new HttpEntity<>(form, headers);
 
-    private HttpHeaders createHeaders() {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-        return headers;
-    }
+			return restTemplate.exchange(azureProperties.getTokenUrl(), HttpMethod.POST, requestEntity, TokenResponse.class)
+					.getBody();
+		} catch (HttpClientErrorException | HttpServerErrorException e) {
+			throw new AzureTokenException(String.format("Klarte ikke hente token fra Azure. Feilet med httpstatus=%s. Feilmelding=%s", e.getStatusCode(), e.getMessage()), e);
+		}
+	}
+
+	private HttpHeaders createHeaders() {
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+		headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+		return headers;
+	}
 }
