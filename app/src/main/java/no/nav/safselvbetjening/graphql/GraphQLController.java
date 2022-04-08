358cfdac6cffb720d679bdca83d1f7cf616d386d
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2022-04-08 09:55:36 +0200

    MMA-6083 La til query cache på graphql query parsing.

--- app/src/main/java/no/nav/safselvbetjening/graphql/GraphQLController.java
+++ app/src/main/java/no/nav/safselvbetjening/graphql/GraphQLController.java
@@ -1,8 +1,10 @@
 package no.nav.safselvbetjening.graphql;
 
+import com.github.benmanes.caffeine.cache.Cache;
 import graphql.ExecutionInput;
 import graphql.ExecutionResult;
 import graphql.GraphQL;
+import graphql.execution.preparsed.PreparsedDocumentEntry;
 import graphql.schema.GraphQLSchema;
 import graphql.schema.idl.SchemaGenerator;
 import graphql.schema.idl.SchemaParser;
@@ -11,6 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 import no.nav.security.token.support.core.api.Protected;
 import no.nav.security.token.support.core.context.TokenValidationContextHolder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,10 +23,12 @@ import org.springframework.web.context.request.WebRequest;
 import java.io.InputStreamReader;
 import java.util.Collections;
 import java.util.Map;
-import java.util.Objects;
 import java.util.UUID;
+import java.util.function.Function;
 
+import static java.util.Objects.requireNonNull;
 import static no.nav.safselvbetjening.NavHeaders.NAV_CALLID;
+import static no.nav.safselvbetjening.cache.CacheConfig.GRAPHQL_QUERY_CACHE;
 import static no.nav.safselvbetjening.graphql.GraphQLRequestContext.KEY;
 import static org.apache.commons.lang3.StringUtils.isNotBlank;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
@@ -39,23 +44,34 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 public class GraphQLController {
 	private final GraphQLSchema graphQLSchema;
 	private final TokenValidationContextHolder tokenValidationContextHolder;
+	private final Cache<String, PreparsedDocumentEntry> cache;
 
+	@SuppressWarnings("unchecked")
 	@Autowired
-	public GraphQLController(GraphQLWiring graphQLWiring, TokenValidationContextHolder tokenValidationContextHolder) {
+	public GraphQLController(GraphQLWiring graphQLWiring,
+							 TokenValidationContextHolder tokenValidationContextHolder,
+							 CacheManager cacheManager) {
 		this.tokenValidationContextHolder = tokenValidationContextHolder;
 		SchemaParser schemaParser = new SchemaParser();
-		InputStreamReader schema = new InputStreamReader(Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream("schemas/safselvbetjening.graphqls")));
+		InputStreamReader schema = new InputStreamReader(requireNonNull(getClass().getClassLoader().getResourceAsStream("schemas/safselvbetjening.graphqls")));
 
 		TypeDefinitionRegistry typeRegistry = schemaParser.parse(schema);
 		SchemaGenerator schemaGenerator = new SchemaGenerator();
 		this.graphQLSchema = schemaGenerator.makeExecutableSchema(typeRegistry, graphQLWiring.createRuntimeWiring());
+		this.cache = (Cache<String, PreparsedDocumentEntry>) requireNonNull(cacheManager.getCache(GRAPHQL_QUERY_CACHE)).getNativeCache();
 	}
 
 	@PostMapping(value = "/graphql", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
 	@ResponseBody
 	public Map<String, Object> graphQLRequest(@RequestBody final GraphQLRequest request, final WebRequest webRequest) {
 		ExecutionResult executionResult =
-				GraphQL.newGraphQL(graphQLSchema).build()
+				GraphQL.newGraphQL(graphQLSchema)
+						// https://www.graphql-java.com/documentation/execution/#query-caching
+						.preparsedDocumentProvider((executionInput, parseAndValidateFunction) -> {
+							Function<String, PreparsedDocumentEntry> mapCompute = key -> parseAndValidateFunction.apply(executionInput);
+							return cache.get(executionInput.getQuery(), mapCompute);
+						})
+						.build()
 						.execute(ExecutionInput.newExecutionInput()
 								.query(request.getQuery())
 								.operationName(request.getOperationName())
