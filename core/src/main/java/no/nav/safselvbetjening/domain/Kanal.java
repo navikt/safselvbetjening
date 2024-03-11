37bf61096d28e26806193a873a52886ff809aa5e
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2024-03-11 15:48:44 +0100

    MMA-7336 Oppdatert kanalnavn for DPV
    * DPV - Taushetsbelagt Post via Altinn

--- core/src/main/java/no/nav/safselvbetjening/domain/Kanal.java
+++ core/src/main/java/no/nav/safselvbetjening/domain/Kanal.java
@@ -21,7 +21,7 @@ public enum Kanal {
 	NAV_NO_UINNLOGGET("Ditt NAV uten ID-porten-pålogging"),
 	INNSENDT_NAV_ANSATT("Registrert av Nav-ansatt"),
 	NAV_NO_CHAT("Innlogget samtale"),
-	DPV("Digital Post til Virksomhet"),
+	DPV("Taushetsbelagt Post via Altinn"),
 	E_POST("E-post"),
 	ALTINN_INNBOKS("Altinn Innboks");
 
