e05c8d62799510e70b5d6ab1028c4ac73e5112f9
Anders Opskar Voldsund <anders.voldsund@nav.no>
2025-09-09 08:25:26 +0200

    MMA-8121 Fiks validering av properties som ikkje fungerte
    Fjern også kommentarar som ikkje gir så mykje verdi.

--- core/src/main/java/no/nav/safselvbetjening/SafSelvbetjeningProperties.java
+++ core/src/main/java/no/nav/safselvbetjening/SafSelvbetjeningProperties.java
@@ -1,5 +1,6 @@
 package no.nav.safselvbetjening;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
@@ -11,94 +12,70 @@ import org.springframework.validation.annotation.Validated;
 @Validated
 public class SafSelvbetjeningProperties {
 
+    @Valid
     private final Endpoints endpoints = new Endpoints();
+    @Valid
     private final Topics topics = new Topics();
+
     private final Feature feature = new Feature();
 
     @Data
-    @Validated
     public static class Endpoints {
-        /**
-         * URL til PDL (Persondataløsningen).
-         */
         @NotNull
+        @Valid
         private AzureEndpoint pdl;
 
-        /**
-         * URL til sak API.
-         */
         @NotNull
+        @Valid
         private AzureEndpoint sak;
 
-        /**
-         * URL til oppslagstjenesten i fagarkivet.
-         */
-        @NotEmpty
-        private String fagarkiv;
-
-        /**
-         * URL til safintern oppslagstjenesten i dokarkiv.
-         */
         @NotNull
+        @Valid
         private AzureEndpoint dokarkiv;
 
-        /**
-         * URL til PEN (pensjon).
-         */
         @NotNull
+        @Valid
         private AzureEndpoint pensjon;
 
         @NotNull
+        @Valid
         private TokenXEndpoint reprApi;
+
+        @NotEmpty
+        private String fagarkiv;
     }
 
     @Data
-    @Validated
     public static class AzureEndpoint {
-        /**
-         * Url til tjeneste som har azure autorisasjon
-         */
         @NotEmpty
         private String url;
-        /**
-         * Scope til azure client credential flow
-         */
+
         @NotEmpty
         private String scope;
     }
 
     @Data
-    @Validated
     public static class TokenXEndpoint {
-        /**
-         * Url til tjeneste som har tokenx autorisasjon
-         */
         @NotEmpty
         private String url;
-        /**
-         * Scope til tokenx exchange flow
-         */
+
         @NotEmpty
         private String scope;
     }
 
     @Data
-    @Validated
     public static class Topics {
-    	/**
-    	 * Kafka topic for kommunikasjon mot dokdistdittnav
-    	 */
         @NotEmpty
         private String dokdistdittnav;
 
     }
 
     @Data
-    @Validated
     public static class Feature {
         /**
          * Aktiverer innsyn for alle PEN og UFO journalposter
          */
         private boolean mma7514;
     }
+
 }
