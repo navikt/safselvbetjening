6395dc4fd379dd60c3956de6768c2c4829f8a52a
Kristian Laskemoen <Kristian.Laskemoen@nav.no>
2026-02-17 14:53:55 +0100

    nye bucketnavn

--- core/src/main/java/no/nav/safselvbetjening/metrics/DokumentCounter.java
+++ core/src/main/java/no/nav/safselvbetjening/metrics/DokumentCounter.java
@@ -20,11 +20,11 @@ public class DokumentCounter {
 
 	@Autowired
 	public DokumentCounter(MeterRegistry meterRegistry) {
-		bucket0_6 = meterRegistry.counter("dok_alder_bucket", "range", "0-6");
-		bucket7_12 = meterRegistry.counter("dok_alder_bucket", "range", "712");
-		bucket13_24 = meterRegistry.counter("dok_alder_bucket", "range", "13_24");
-		bucket25_60 = meterRegistry.counter("dok_alder_bucket", "range", "25_60");
-		bucket60plus = meterRegistry.counter("dok_alder_bucket", "range", "60plus");
+		bucket0_6 = meterRegistry.counter("dok_alder_months_bucket", "range", "0-6");
+		bucket7_12 = meterRegistry.counter("dok_alder_months_bucket", "range", "712");
+		bucket13_24 = meterRegistry.counter("dok_alder_months_bucket", "range", "13_24");
+		bucket25_60 = meterRegistry.counter("dok_alder_months_bucket", "range", "25_60");
+		bucket60plus = meterRegistry.counter("dok_alder_months_bucket", "range", "60plus");
 
 	}
 
